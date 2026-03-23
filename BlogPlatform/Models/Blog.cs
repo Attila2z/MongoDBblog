d884c0eaca67e96e3fa92fde1893a6d4fa4cce74
attil <attil@GEPATI>
2026-03-23 01:21:14 +0100

    Add domain models: Blog, Post, Comment

---
+++ BlogPlatform/Models/Blog.cs
@@ -0,0 +1,14 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+public class Blog
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string UserId { get; set; } = null!;
+}
---
+++ BlogPlatform/Models/Comment.cs
@@ -0,0 +1,6 @@
+public class Comment
+{
+    public string AuthorName { get; set; } = null!;
+    public string Body { get; set; } = null!;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
